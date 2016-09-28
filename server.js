16f88720a93534007821e7079797d4235f9e6421
renju1234 <renjithkumarb84@gmail.com>
2016-09-28 10:11:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'travancore.html'));
 });
 
 app.get("/article-one", function(req, res){
