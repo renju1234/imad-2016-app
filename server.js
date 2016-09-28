b2afd5822cb7408a2900e56b405c3fae48a5899c
renju1234 <renjithkumarb84@gmail.com>
2016-09-28 11:09:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,6 +24,10 @@ app.get("/article-three", function(req, res){
    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
+app.get("/travancorehydrotech", function(req, res){
+   res.sendFile(path.join(__dirname, 'ui', 'travancorehydrotech.html'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
