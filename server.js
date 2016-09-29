e39480794371a1093590dae035923472ba0f7466
renju1234 <renjithkumarb84@gmail.com>
2016-09-29 11:37:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,12 +4,63 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-app.get('/', function (req, res) {
+
+var articleone = {
+ title:'articleone i am renju',
+ heading: 'article one',
+ date: '28 sept 2016',
+ content:
+       `<p>
+             This is the content for the first article.This is the content for the first article.This is the content for the first article.This is the content for the first article.This is the content for the first article.This is the content for the first article.This is the content for the first article.This is the content for the first article.
+        </p>   
+        <p>
+            This is the content for the first article.This is the content for the first article.This is the content for the second article.This is the content for the first article.This is the content for the first article.This is the content for the first article.This is the content for the first article.This is the content for the first article.   
+        </p>   
+        <p>
+            This is the content for the first article.This is the content for the first article.This is the content for the first article.This is the content for the first article.This is the content for the first article.This is the content for the first article.This is the content for the first article.This is the content for the first article.
+        </p>    
+        <p>
+           This is the content for the second article.This is the content for the first article.This is the content for the second article.This is the content for the first article.This is the content for the first article.This is the content for the first article.This is the content for the first article.This is the content for the first article. 
+        </p>`
+  };
+ function createtemplate (date)(
+var title = data.title:
+var date = data.date:
+var heading = data.heading:
+var content = data.content:
+
+  var htmltemplete = {<html>
+   <head>
+       <title>
+           ${title}
+         </title> 
+     <link href="/ui/style.css" rel="stylesheet" />
+  </head>
+   <body>
+    <div class="container">
+       <div>
+           <a href="/"a>home</a>
+       </div>
+       <hr/>
+       <h3>
+          ${heading}
+       </h3>
+       <div>
+           ${date}
+       </div>
+           ${content}
+       </div>
+      </body>
+   </html>
+  };
+  return htmltemplate
+ )
+  app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get("/article-one", function(req, res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   res.send(createTemplate(articleone));
 });
 
 app.get("/article-two", function(req, res){
